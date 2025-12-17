56f0075adfd89b5c6dcc966ea0b2b9b3eb98a0ee
sriramaravindh03 <Sriram.Aravindh@cognizant.com>
2025-12-17 16:41:14 +0530

    Added home page and post mapping

--- src/main/java/com/genc/pams/controller/PatientController.java
+++ src/main/java/com/genc/pams/controller/PatientController.java
@@ -3,18 +3,26 @@ package com.genc.pams.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.genc.pams.model.Patient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PatientController {
-    @GetMapping("/test")
+    @GetMapping("/register")
     public String show(Model model) {
-        String name = "Patient 1";
-        model.addAttribute("patientName",name);
-        return "abc";
+        // String name = "Patient 1";
+        // Patient p = new Patient();
+        model.addAttribute("patient",new Patient());
+        return "registerPatient";
     }
-    // Hello world
-    //hello tamizh selvan
-
+    
+    @PostMapping("/home")
+    public String registerPatient(@ModelAttribute Patient patient, Model model) {
+        String name = patient.getPatientName();
+        String email = patient.getEmail();
+        String out = "Patient Name: "+name+"\n"+"Email: "+email;
+        model.addAttribute("out", out);
+        return "patientHome";
+    }
 }
--- src/main/java/com/genc/pams/model/Patient.java
+++ src/main/java/com/genc/pams/model/Patient.java
@@ -9,10 +9,44 @@ public class Patient {
 	private int patientID;
 	private String patientName;
 	private long phno;
+	private String email;
+	private String address;
 	public Patient() {}
-	public Patient(String patientName, long phno) {
-		super();
+	public Patient(String patientName, long phno, String email, String address) {
 		this.patientName = patientName;
 		this.phno = phno;
+		this.email = email;
+		this.address = address;
 	}
+	public int getPatientID() {
+		return patientID;
+	}
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
