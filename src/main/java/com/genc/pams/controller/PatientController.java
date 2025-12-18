a29a890f13173a50afae71573bc1a85f6e338a10
sriramaravindh03 <Sriram.Aravindh@cognizant.com>
2025-12-18 15:38:11 +0530

    Added patient registration saving functionality

--- src/main/java/com/genc/pams/controller/PatientController.java
+++ src/main/java/com/genc/pams/controller/PatientController.java
@@ -1,24 +1,29 @@
 package com.genc.pams.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import com.genc.pams.model.Patient;
+import com.genc.pams.service.PatientService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
     @GetMapping("/register")
     public String show(Model model) {
-        // String name = "Patient 1";
-        // Patient p = new Patient();
         model.addAttribute("patient",new Patient());
         return "registerPatient";
     }
     
     @PostMapping("/home")
     public String registerPatient(@ModelAttribute Patient patient, Model model) {
+        patientService.savePatient(patient);
         String name = patient.getPatientName();
         String email = patient.getEmail();
         String out = "Patient Name: "+name+"\n"+"Email: "+email;
--- src/main/java/com/genc/pams/service/PatientService.java
+++ src/main/java/com/genc/pams/service/PatientService.java
@@ -1,8 +1,17 @@
 package com.genc.pams.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.genc.pams.model.Patient;
+import com.genc.pams.repository.PatientRepository;
 
 @Service
 public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
 
+    public void savePatient(Patient patient) {
+        patientRepository.save(patient);
+    }
 }
